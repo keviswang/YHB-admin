2337754138b83b50032e282f5d9b513396c6df0f
pyt111 <584702675@qq.com>
2018-01-17 23:40:52 +0800

    日期

--- src/components/customTemplate/search/inputList.vue
+++ src/components/customTemplate/search/inputList.vue
@@ -1,14 +1,14 @@
 <template>
-    <div>
-        <Row :gutter="26">
-            <Col v-for="(x,index) in tableArr" :xs="x.colSize.xs" :sm="x.colSize.sm" :lg="x.colSize.lg" :key="x.id" class="inputList">
-            <DatePicker  class="timeinput" v-if="x.kind == 'date'" :id="x.id" :type="x.type" :options="options1" :placeholder="x.placeholder" style="width: 100%;height:100%;"></DatePicker>
-            <Input v-model="tableArr[index].val" v-if="x.kind == 'textInput'" :id="x.id" :placeholder="x.placeholder" style="width: 100%" @on-enter="met(x,index)">
-            <span slot="prepend">{{x.name}}</span>
-            </Input>
-            </Col>
-        </Row>
-    </div>
+	<div>
+		<Row :gutter="26">
+			<Col v-for="(x,index) in tableArr" :xs="x.colSize.xs" :sm="x.colSize.sm" :lg="x.colSize.lg" :key="x.id" class="inputList">
+			<DatePicker v-model="tableArr[index].val" class="timeinput" v-if="x.kind == 'date'" :id="x.id" :type="x.type" :options="options1" :placeholder="x.placeholder" style="width: 100%;height:100%;" format="yyyy-MM-dd" @on-change="asd($event,x,index)"></DatePicker>
+			<Input v-model="tableArr[index].val" v-if="x.kind == 'textInput'" :id="x.id" :placeholder="x.placeholder" style="width: 100%" @on-change="met(x,index)">
+			<span slot="prepend">{{x.name}}</span>
+			</Input>
+			</Col>
+		</Row>
+	</div>
 </template>
 
 <script>
@@ -58,21 +58,28 @@ export default {
             }
         };
     },
-    mounted() {
-        
-    },
+    mounted() {},
     methods: {
-        met(x,index) {
-            console.log(x.val);
-            for(let x in this.tableArr) {
-                
+        met(x, index) {
+            let putDataObj = new Object();
+            for (let y in this.tableArr) {
+                putDataObj[this.tableArr[y].id] = this.tableArr[y].val;
             }
+            console.log(putDataObj);
+        },
+        asd($event,x,index) {
+            console.log($event,x,index);
+			
+            let putDataObj = new Object();
+            for (let y in this.tableArr) {
+                console.log(this.tableArr[index]);
+                putDataObj[this.tableArr[y].id] = this.tableArr[index].val;
+				
+            }
+			console.log(putDataObj);
         }
-       
     },
-    computed: {
-
-    }
+    computed: {}
 };
 </script>
 
@@ -84,6 +91,5 @@ export default {
         margin: 0 auto;
     }
     @include medias();
-    
 }
 </style>
